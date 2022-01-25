63d74f09e4f97e7b47f5ebd66293598efc45f3c1
CalculusCat95 <90402170+CalculusCat95@users.noreply.github.com>
2022-01-25 11:15:59 +0200

    added stuff to the test app

--- Server/test apps/served.py
+++ Server/test apps/served.py
@@ -9,3 +9,7 @@ print("3: Do you have a good ravioli recipe?")
 question1 = input("Enter number for responce(1,2,or 3)")
 if question1 == 1:
   print("Oh its been so long I can hardly remember. The icy winds freeze me to my core. The bitter taste of canned ravioli is my only comfort. I don't remember how I got this way. All I can remember now is the tart sauce, and those creamy pouches. They quench my undying hunger but do not satisfy me.")
+if question1 == 2:
+  print("You don't eat enough ravioli")
+if question1 == 3:
+  print("SPEAK NOT THE NAME OF FRESH RAVIOLI! Come, young one. Come to the dark side. Join me, let the creamy pouches curl around your vitals.")
