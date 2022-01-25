11807ca01e7d81641b0271bfcf6bed2623f21dc4
Jon S <68835972+C0LateralD4mage@users.noreply.github.com>
2022-01-25 15:29:02 +0000

    Updated choose your own adventure type game slightly.

--- Server/test apps/served.py
+++ Server/test apps/served.py
@@ -1,6 +1,9 @@
 print("y0u 4re H4ck")
-scaredyou = input("Enter Password:")
-print("ravioli")
+print("Enter password to recover files.")
+scaredyou = input("Password:")
+print("User:ravioli")
+print("Password:"+scaredyou)
+print("Console Login Successful!")
 print("Here on this snowy peak, I get my internet through a single strand of copper wire. I've grown lonely over the years.")
 print("Enter response number:")
 print("1: How long have you been up there?")
@@ -10,6 +13,6 @@ question1 = input("Enter number for responce(1,2,or 3)")
 if question1 == 1:
   print("Oh its been so long I can hardly remember. The icy winds freeze me to my core. The bitter taste of canned ravioli is my only comfort. I don't remember how I got this way. All I can remember now is the tart sauce, and those creamy pouches. They quench my undying hunger but do not satisfy me.")
 if question1 == 2:
-  print("You don't eat enough ravioli")
+  print("ravioli")
 if question1 == 3:
   print("SPEAK NOT THE NAME OF FRESH RAVIOLI! Come, young one. Come to the dark side. Join me, let the creamy pouches curl around your vitals.")
